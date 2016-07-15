dd8b9bad91f6d0706d79c73c50e33f36474c3acd
gcr <390322114@qq.com>
2016-07-15 09:55:17 +0800

    Initial Commit

---
+++ CoreData01/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CoreData01
+//
+//  Created by onlyou1991 on 16/7/15.
+//  Copyright © 2016年 onlyou1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CoreData01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData01
+//
+//  Created by onlyou1991 on 16/7/15.
+//  Copyright © 2016年 onlyou1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
